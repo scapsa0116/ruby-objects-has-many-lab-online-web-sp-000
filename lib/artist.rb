a285b448538abcb267a0b65800bcb3285bf411fe
Stela Capsa <stela.capsa@gmail.com>
2020-01-06 21:03:57 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require 'pry'
+class Artist
+  attr_accessor :name
+
+ @@song_count = 0
+  
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+   
+  end
+
+  def songs
+  songs.add_song_by_name
+   
+  end
+  
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+def song_count
+   Song.artist.count
+  end
+
+end 
